d557e0545c085a00c05b2ff754932fb48bd5f26b
jeffrey-thiers <jeffrey@thiers.us>
2026-01-10 12:44:31 -0800

    automated the horizontal grid lines

--- screen_test_read_voltage/screen_test_read_voltage.ino
+++ screen_test_read_voltage/screen_test_read_voltage.ino
@@ -15,6 +15,8 @@ int oldSamples[bufferSize];
 
 
 void setup() {
+  Serial.begin(9600); // Set baud rate (bits per second)
+  
   tft.begin(8000000L); // Force max SPI speed (8MHz) for Nano
   tft.setRotation(1);
   tft.fillScreen(ILI9341_BLACK);
@@ -27,24 +29,22 @@ void setup() {
   analogWrite(3,127);
 
   // Draw grid
-  tft.setCursor(1, 32);
-  tft.setTextColor(ILI9341_RED);
-  tft.setTextSize(1);
-  tft.println("5V");
-  tft.drawFastHLine(0, 40, 320, ILI9341_RED);
-
-  tft.setCursor(1, 112);
-  tft.setTextColor(ILI9341_RED);
-  tft.setTextSize(1);
-  tft.println("2.5V");
-  tft.drawFastHLine(0, 120, 320, ILI9341_RED);
-
-  tft.setCursor(1, 192);
-  tft.setTextColor(ILI9341_RED);
-  tft.setTextSize(1);
-  tft.println("0V");
-  tft.drawFastHLine(0, 201, 320, ILI9341_RED);
-    
+  //tft.setCursor(1, 32);
+  //tft.setTextColor(ILI9341_RED);
+  //tft.setTextSize(1);
+  //tft.println("5V");
+  //tft.drawFastHLine(0, 40, 320, ILI9341_RED);
+  //tft.setCursor(1, 112);
+  //tft.setTextColor(ILI9341_RED);
+  //tft.setTextSize(1);
+  //tft.println("2.5V");
+  //tft.drawFastHLine(0, 120, 320, ILI9341_RED);
+  //tft.setCursor(1, 192);
+  //tft.setTextColor(ILI9341_RED);
+  //tft.setTextSize(1);
+  //tft.println("0V");
+  //tft.drawFastHLine(0, 201, 320, ILI9341_RED);
+  drawGrid(6);
 }
 
 
@@ -72,5 +72,17 @@ void loop() {
     
     // Store for next erase cycle
     oldSamples[i] = samples[i];
+  }
+}
+
+
+void drawGrid(int num_div) {
+  int pix_per_div = 240/num_div;
+  int cursor = -1;//some reason it can't plot at certain even values..
+  for(int i = 0; i < num_div-1; i += 1){
+    cursor += pix_per_div;
+    //Serial.print("drawing line at: ");
+    //Serial.println(cursor);
+    tft.drawFastHLine(0, cursor, 320, ILI9341_RED);
   }
 }
